c85801fc4077ddd7e025b3c27b7066cf365a73a8
Jared Ostmeyer <jostmey@gmail.com>
2022-10-05 17:43:24 -0500

    Adding job script

---
+++ cervical-cancer/jobs.sh
@@ -0,0 +1,98 @@
+mkdir -p bin
+
+# Split 1
+#
+python3 train_val.py --seed 1 --holdouts 112015051_3_38 --output bin/1 > bin/train_val_1.out
+
+# Split 2
+#
+python3 train_val.py --seed 1 --holdouts 3-4_DNA --output bin/2 > bin/train_val_2.out
+
+# Split 3
+#
+python3 train_val.py --seed 1 --holdouts 5-15_DNA --output bin/3 > bin/train_val_3.out
+
+# Split 4
+#
+python3 train_val.py --seed 1 --holdouts 112015051_4_38 --output bin/4 > bin/train_val_4.out
+
+# Split 5
+#
+python3 train_val.py --seed 1 --holdouts 2_31 --output bin/5 > bin/train_val_5.out
+
+# Split 6
+#
+python3 train_val.py --seed 1 --holdouts 112015051_3_39 --output bin/6 > bin/train_val_6.out
+
+# Split 7
+#
+python3 train_val.py --seed 1 --holdouts 4-1_DNA --output bin/7 > bin/train_val_7.out
+
+# Split 8
+#
+python3 train_val.py --seed 1 --holdouts 3-11_DNA --output bin/8 > bin/train_val_8.out
+
+# Split 9
+#
+python3 train_val.py --seed 1 --holdouts 5-6_DNA --output bin/9 > bin/train_val_9.out
+
+# Split 10
+#
+python3 train_val.py --seed 1 --holdouts 112015051_5_33 --output bin/10 > bin/train_val_10.out
+
+# Split 11
+#
+python3 train_val.py --seed 1 --holdouts 3-6_DNA --output bin/11 > bin/train_val_11.out
+
+# Split 12
+#
+python3 train_val.py --seed 1 --holdouts 112015051_5_31 --output bin/12 > bin/train_val_12.out
+
+# Split 13
+#
+python3 train_val.py --seed 1 --holdouts 112015051_5_39 --output bin/13 > bin/train_val_13.out
+
+# Split 14
+#
+python3 train_val.py --seed 1 --holdouts 4-2_DNA --output bin/14 > bin/train_val_14.out
+
+# Split 15
+#
+python3 train_val.py --seed 1 --holdouts 112015051_3_40 --output bin/15 > bin/train_val_15.out
+
+# Split 16
+#
+python3 train_val.py --seed 1 --holdouts 4-13_DNA --output bin/16 > bin/train_val_16.out
+
+# Split 17
+#
+python3 train_val.py --seed 1 --holdouts 5-19_DNA --output bin/17 > bin/train_val_17.out
+
+# Split 18
+#
+python3 train_val.py --seed 1 --holdouts 4-22_DNA --output bin/18 > bin/train_val_18.out
+
+# Split 19
+#
+python3 train_val.py --seed 1 --holdouts 112015051_4_33 --output bin/19 > bin/train_val_19.out
+
+# Split 20
+#
+python3 train_val.py --seed 1 --holdouts 2-30_DNA --output bin/20 > bin/train_val_20.out
+
+# Split 21
+#
+python3 train_val.py --seed 1 --holdouts 5-27A_DNA --output bin/21 > bin/train_val_21.out
+
+# Split 22
+#
+python3 train_val.py --seed 1 --holdouts 112015051_3_32 --output bin/22 > bin/train_val_22.out
+
+# Split 23
+#
+python3 train_val.py --seed 1 --holdouts 112015051_5_35 --output bin/23 > bin/train_val_23.out
+
+# Split 24
+#
+python3 train_val.py --seed 1 --holdouts 4-11_DNA --output bin/24 > bin/train_val_24.out
+
