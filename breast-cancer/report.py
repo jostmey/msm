6727d565e7a061cdb016285f45e3338251d36a6b
Jared Ostmeyer <jostmey@gmail.com>
2022-10-15 23:45:04 -0500

    Fixing bug in script for reporting results

--- breast-cancer/report.py
+++ breast-cancer/report.py
@@ -18,51 +18,121 @@ import numpy as np
 # Load data
 ##########################################################################################
 
-steps_train = {}
+costs_train = {}
+accuracies_train = {}
+tprs_train = {}
+fprs_train = {}
 for path in glob.glob('bin/*_ps_train.csv'):
-  filename = path.split('/')[-1].split('.')[0]
-  index, step, _, _ = filename.split('_')
-  index_ = int(index)
-  step_ = int(step)
-  cost = 0.0
-  accuracy = 0.0
   with open(path, 'r') as stream:
     reader = csv.DictReader(stream)
+    costs = []
+    accuracies = []
+    tprs = []
+    fprs = []
     for row in reader:
-      subject = row['Subject']
       label = float(row['Label'])
       weight = float(row['Weight'])
       prediction = float(row['Prediction'])
-      cost += weight*(-xlogy(label, prediction)-xlogy(1.0-label, 1.0-prediction))/np.log(2.0)
-      accuracy += weight*100.0*np.equal(np.round(label), np.round(prediction)).astype(np.float64)
-  if step_ not in steps_train:
-    steps_train[step_] = {}
-  if label not in steps_train[step_]:
-    steps_train[step_][label] = {}
-  steps_train[step_][label][index_] = { 'accuracy': accuracy, 'cost': cost }
+      costs.append(
+        weight*(-xlogy(label, prediction)-xlogy(1.0-label, 1.0-prediction))/np.log(2.0)
+      )
+      accuracies.append(
+        100.0*weight*np.equal(np.round(label), np.round(prediction)).astype(np.float64)
+      )
+      if label == 1.0:
+        tprs.append(
+          100.0*np.equal(np.round(label), np.round(prediction)).astype(np.float64)
+        )
+      elif label == 0.0:
+        fprs.append(
+          100.0*np.equal(np.round(label), np.round(prediction)).astype(np.float64)
+        )
+      else:
+        print('ERROR: Unrecognized value in the label.')
+        exit()
+  filename = path.split('/')[-1].split('.')[0]
+  _, step, _, _ = filename.split('_')
+  i = int(step)
+  if i not in costs_train:
+    costs_train[i] = []
+  costs_train[i].append(
+    np.sum(costs)
+  )
+  if i not in accuracies_train:
+    accuracies_train[i] = []
+  accuracies_train[i].append(
+    np.sum(accuracies)
+  )
+  if len(tprs) > 0:
+    if i not in tprs_train:
+      tprs_train[i] = []
+    tprs_train[i].append(
+      np.mean(tprs)
+    )
+  if len(fprs) > 0:
+    if i not in fprs_train:
+      fprs_train[i] = []
+    fprs_train[i].append(
+      np.mean(fprs)
+    )
 
-steps_val = {}
+costs_val = {}
+accuracies_val = {}
+tprs_val = {}
+fprs_val = {}
 for path in glob.glob('bin/*_ps_val.csv'):
-  filename = path.split('/')[-1].split('.')[0]
-  index, step, _, _ = filename.split('_')
-  index_ = int(index)
-  step_ = int(step)
-  cost = 0.0
-  accuracy = 0.0
   with open(path, 'r') as stream:
     reader = csv.DictReader(stream)
+    costs = []
+    accuracies = []
+    tprs = []
+    fprs = []
     for row in reader:
-      subject = row['Subject']
       label = float(row['Label'])
       weight = float(row['Weight'])
       prediction = float(row['Prediction'])
-      cost += weight*(-xlogy(label, prediction)-xlogy(1.0-label, 1.0-prediction))/np.log(2.0)
-      accuracy += weight*100.0*np.equal(np.round(label), np.round(prediction)).astype(np.float64)
-  if step_ not in steps_val:
-    steps_val[step_] = {}
-  if label not in steps_val[step_]:
-    steps_val[step_][label] = {}
-  steps_val[step_][label][index_] = { 'accuracy': accuracy, 'cost': cost }
+      costs.append(
+        weight*(-xlogy(label, prediction)-xlogy(1.0-label, 1.0-prediction))/np.log(2.0)
+      )
+      accuracies.append(
+        100.0*weight*np.equal(np.round(label), np.round(prediction)).astype(np.float64)
+      )
+      if label == 1.0:
+        tprs.append(
+          100.0*np.equal(np.round(label), np.round(prediction)).astype(np.float64)
+        )
+      elif label == 0.0:
+        fprs.append(
+          100.0*np.equal(np.round(label), np.round(prediction)).astype(np.float64)
+        )
+      else:
+        print('ERROR: Unrecognized value in the label.')
+        exit()
+  filename = path.split('/')[-1].split('.')[0]
+  _, step, _, _ = filename.split('_')
+  i = int(step)
+  if i not in costs_val:
+    costs_val[i] = []
+  costs_val[i].append(
+    np.sum(costs)
+  )
+  if i not in accuracies_val:
+    accuracies_val[i] = []
+  accuracies_val[i].append(
+    np.sum(accuracies)
+  )
+  if len(tprs) > 0:
+    if i not in tprs_val:
+      tprs_val[i] = []
+    tprs_val[i].append(
+      np.mean(tprs)
+    )
+  if len(fprs) > 0:
+    if i not in fprs_val:
+      fprs_val[i] = []
+    fprs_val[i].append(
+      np.mean(fprs)
+    )
 
 ##########################################################################################
 # Results
@@ -70,54 +140,14 @@ for path in glob.glob('bin/*_ps_val.csv'):
 
 print(
   'Step',
-  ','.join([ 'Train_Accuracy_'+str(int(label)) for label in sorted(steps_train[32].keys()) ]),
-  'Train_Accuracy',
-  'Train_Cost',
-  ','.join([ 'Val_Accuracy_'+str(int(label)) for label in sorted(steps_val[32].keys()) ]),
-  'Val_Accuracy',
-  'Val_Cost',
+  'Accuracy_Train', 'TRP_Train', 'FPR_Train', 'Cost_train',
+  'Accuracy_Val', 'TRP_Val', 'FPR_Val', 'Cost_Val',
   sep=','
 )
-
-for step in sorted(steps_train.keys()):
-
-  labels = steps_train[step]
-  costs_train = []
-  accuracies_train = []
-  for label in sorted(labels.keys()):
-    cost_total = 0.0
-    accuracy_total = 0.0
-    count_total = 0.0
-    indices = labels[label]
-    for index, metrics in indices.items():
-      cost_total += metrics['cost']
-      accuracy_total += metrics['accuracy']
-      count_total += 1.0
-    costs_train.append(cost_total/count_total)
-    accuracies_train.append(accuracy_total/count_total)
-
-  labels = steps_val[step]
-  costs_val = []
-  accuracies_val = []
-  for label in sorted(labels.keys()):
-    cost_total = 0.0
-    accuracy_total = 0.0
-    count_total = 0.0
-    indices = labels[label]
-    for index, metrics in indices.items():
-      cost_total += metrics['cost']
-      accuracy_total += metrics['accuracy']
-      count_total += 1.0
-    costs_val.append(cost_total/count_total)
-    accuracies_val.append(accuracy_total/count_total)
-
+for i in sorted(accuracies_train.keys()):
   print(
-    step,
-    ','.join([ str(accuracy) for accuracy in accuracies_train ]),
-    np.mean(accuracies_train),
-    np.mean(costs_train),
-    ','.join([ str(accuracy) for accuracy in accuracies_val ]),
-    np.mean(accuracies_val),
-    np.mean(costs_val),
+    i,
+    np.mean(accuracies_train[i]), np.mean(tprs_train[i]), np.mean(fprs_train[i]), np.mean(costs_train[i]),
+    np.mean(accuracies_val[i]), np.mean(tprs_val[i]), np.mean(fprs_val[i]), np.mean(costs_val[i]),
     sep=','
   )
